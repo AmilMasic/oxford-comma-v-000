1b21f98238105788b8a748a0267928db90123a95
Amil Masic <amil.masic@gmail.com>
2018-09-11 15:15:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size <= 3
+  elsif array.size = 3
     last_item = array.last
     array.pop
     array.join(", ")
-    
+
   end
 end
