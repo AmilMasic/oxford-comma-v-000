fd47b6ad354aa86850e432a1d3c10d67fcca1009
Amil Masic <amil.masic@gmail.com>
2018-09-11 14:56:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,6 @@
 def oxford_comma(array)
   # array = ["fiddleheads", "okra", "kohlrabi"]
-case array
-  while array.size == 1
+  if array.size == 1
     array.join
-    while array.size == 2
-      array.join(" and ")
-    end
-  end
   end
 end
